14d52ffcd69caaa612e124eed2066f8aea3911de
Fernando Valverde Arredondo <fdov88@gmail.com>
2017-04-02 20:14:44 +0200

    Fixed symbol params merge issue

--- lib/turrialba/client.rb
+++ lib/turrialba/client.rb
@@ -59,22 +59,22 @@ module Turrialba
 
     private
     def filter_user_params(hash)
-      valid_params = ["screen_name", "name", "location", "description",
-        "website", "profile_link_color", "image_url", "banner_url",
-        "statuses_count", "favourites_count", "followers_count",
-        "friends_count", "listed_count", "protected", "verified", "studder",
-        "followers_with_studder", "follower_mean_studder",
-        "scrapping_queued_at", "scrapping_request_count", "scrapped_at",
-        "possessed_at", "possession_group", "queue_rate_pause",
-        "total_scrapping_request_count"]
+      valid_params = [:screen_name, :name, :location, :description,
+        :website, :profile_link_color, :image_url, :banner_url,
+        :statuses_count, :favourites_count, :followers_count,
+        :friends_count, :listed_count, :protected, :verified, :studder,
+        :followers_with_studder, :follower_mean_studder,
+        :scrapping_queued_at, :scrapping_request_count, :scrapped_at,
+        :possessed_at, :possession_group, :queue_rate_pause,
+        :total_scrapping_request_count]
       hash.select {|k,v| valid_params.include?(k) }
     end
 
     def filter_tweet_params(hash)
-      valid_params = ["id_str", "text", "tweeted_at", "embedded_html",
-        "author_id", "in_reply_to_tweet_id", "scrapping_queued_at",
-        "scrapping_request_count", "scrapped_at", "favorite_count",
-        "retweet_count", "in_reply_to_user_id", "total_scrapping_request_count"]
+      valid_params = [:id_str, :text, :tweeted_at, :embedded_html,
+        :author_id, :in_reply_to_tweet_id, :scrapping_queued_at,
+        :scrapping_request_count, :scrapped_at, :favorite_count,
+        :retweet_count, :in_reply_to_user_id, :total_scrapping_request_count]
       hash.select {|k,v| valid_params.include?(k) }
     end
   end
